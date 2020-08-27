6b4aa8c6c5362ec83da55363e279621d300dc62f
ephie oyedoh <ephieo@rocketmail.com>
2020-08-27 17:22:41 +0100

    merging

--- src/server.js
+++ src/server.js
@@ -12,7 +12,6 @@ server.use(express.json());
 // routes below
 
 // 1. GET home page - see all posts
-<<<<<<< HEAD
 server.get('/', travelposts.getAllPosts);
 // 2. POST user login details
 // server.post("/login", express.urlencoded(),users.login);
@@ -21,22 +20,25 @@ server.post('/signup', express.urlencoded(), users.signUp);
 // 4. POST travel post
 // server.get('/posts/:', )
 // 5. PUT (update) travel post
-=======
-// ver.get('/', travelposts.getAllPosts);
-// 2. POST - user login details
-// server.post('/login', express.urlencoded());
-// 3. POST - sign up
-server.post('/signup', express.urlencoded(), users.signUp);
-
-// 4. POST - travel post
-// server.post('/travels', express.urlencoded());
-
-// 5. PUT - (update) travel post
->>>>>>> 4cf3ac6e196dd0d1170be3c020e58c168c53ecca
 
 // 6. DEL travel post
 
 // 7. GET all posts of one place /posts:place
+// server.get('/posts/:location', travelposts.getLocation);
+
+// 8. GET all posts from one user
+server.get('/posts/:id', travelposts.getId);
+
+// error handling
+
+// server.use(handleErrors);
+
+// listening to server
+server.listen(PORT, () => {
+	console.log(`Listening on http://localhost:${PORT}`);
+});
+
+module.exports = server;
 // server.get('/posts/:location', travelposts.getLocation);
 
 // 8. GET all posts from one user
